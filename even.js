53771b5da13e499b76091f9148bb22ec9be1f25c
SMDAIYAN <shahdaiyan12@gmail.com>
2024-02-08 11:27:27 +0600

    completed even number task from 78 to 98

---
+++ even.js
@@ -0,0 +1,6 @@
+let num = 78
+while (num <= 98) {
+    console.log(num)
+    num = num + 2
+}
+
---
+++ odd.js
@@ -0,0 +1,10 @@
+//  odd number 
+
+let num = 61;
+while (num <=100) {
+    console.log (num)
+    num=num+2
+}
+
+//  even numbers from 78 to 98
+
